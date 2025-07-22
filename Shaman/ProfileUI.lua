c8fba31231cf5ede62fba6ae7ffe63b13cb27ff2
Smegster11 <110310405+Smegster11@users.noreply.github.com>
2025-07-22 22:16:27 +0100

    Layout fix

--- Shaman/ProfileUI.lua
+++ Shaman/ProfileUI.lua
@@ -25,58 +25,58 @@ local LayoutConfigOptions = { gutter = 10, padding = { left = 5, right = 5 } }
 A.Data.ProfileEnabled[A.CurrentProfile] = true
 
 A.Data.ProfileUI = {
-	DateTime = "Smeg's Restoration Shaman v1.0-beta",
-	[2] = {
-		["SHAMAN"] = {
-			LayoutOptions = LayoutConfigOptions,
-		},
-	},
+    DateTime = "Smeg's Restoration Shaman v1.0-beta",
+    [2] = {
+        ["SHAMAN"] = {
+            LayoutOptions = LayoutConfigOptions,
+        },
+    },
 }
 
-local ProfileUI_ShamanTabContent = A.Data.ProfileUI[2]["SHAMAN"]
+local ProfileUI_ShamanTabContent = A.Data.ProfileUI[2] -- This is the line that gets past the 12744 crash.
 
 ----------------------------------------------------------------------
 -----------------------------G E N E R A L----------------------------
 ----------------------------------------------------------------------
 ProfileUI_ShamanTabContent[#ProfileUI_ShamanTabContent + 1] = {
-	{ E = "Header", L = L.SeparatorGeneral },
+    { E = "Header", L = L.SeparatorGeneral },
 }
 
 ProfileUI_ShamanTabContent[#ProfileUI_ShamanTabContent + 1] = {
-	{
-		E = "Checkbox",
-		DB = "mouseover",
-		DBV = true,
-		L = L.UseMouseover,
-		TT = L.UseMouseoverTT,
-		M = {},
-	},
+    {
+        E = "Checkbox",
+        DB = "mouseover",
+        DBV = true,
+        L = L.UseMouseover,
+        TT = L.UseMouseoverTT,
+        M = {},
+    },
 }
 
 ProfileUI_ShamanTabContent[#ProfileUI_ShamanTabContent + 1] = {
-	{
-		E = "Checkbox",
-		DB = "InterruptTargetTarget",
-		DBV = true,
-		L = L.UseTargetTarget,
-		TT = L.UseTargetTargetTT,
-		M = {},
-	},
+    {
+        E = "Checkbox",
+        DB = "InterruptTargetTarget",
+        DBV = true,
+        L = L.UseTargetTarget,
+        TT = L.UseTargetTargetTT,
+        M = {},
+    },
 }
 
 ProfileUI_ShamanTabContent[#ProfileUI_ShamanTabContent + 1] = {
-	{
-		E = "Checkbox",
-		DB = "AoE",
-		DBV = true,
-		L = L.UseAoE,
-		TT = L.UseAoETT,
-		M = {},
-	},
+    {
+        E = "Checkbox",
+        DB = "AoE",
+        DBV = true,
+        L = L.UseAoE,
+        TT = L.UseAoETT,
+        M = {},
+    },
 }
 
 ProfileUI_ShamanTabContent[#ProfileUI_ShamanTabContent + 1] = {
-	{ E = "Dropdown", OT = { { text = "Restoration", value = "Restoration" }, { text = "AUTO", value = "AUTO" } }, DB = "SpecOverride", DBV = "Restoration", L = L.ShamanSpecOverride, TT = L.ShamanSpecOverrideTT, M = {} },
+    { E = "Dropdown", OT = { { text = "Restoration", value = "Restoration" }, { text = "AUTO", value = "AUTO" } }, DB = "SpecOverride", DBV = "Restoration", L = L.ShamanSpecOverride, TT = L.ShamanSpecOverrideTT, M = {} },
 }
 
 ProfileUI_ShamanTabContent[#ProfileUI_ShamanTabContent + 1] = { E = "LayoutSpace" }
@@ -85,16 +85,17 @@ ProfileUI_ShamanTabContent[#ProfileUI_ShamanTabContent + 1] = { E = "LayoutSpace
 --------------------------R E S T O R A T I O N-----------------------
 ----------------------------------------------------------------------
 ProfileUI_ShamanTabContent[#ProfileUI_ShamanTabContent + 1] = {
-	E = "Header", L = L.SeparatorRestoration, M = { hide = function() return A.Data.ProfileDB[A.CurrentProfile][2].SHAMAN.SpecOverride ~= "Restoration" and A.GetCurrentSpec() ~= "RESTO" end },
+    E = "Header", L = L.SeparatorRestoration
 }
 
 ---
 -- ProfileDB Initialization
 A.Data.ProfileDB = {
-	[2] = {
-		["SHAMAN"] = {
-			mouseover = true, InterruptTargetTarget = true, AoE = true,
-			SpecOverride = "Restoration",
-		},
-	},
+    [2] = {
+        ["SHAMAN"] = {
+            mouseover = true, InterruptTargetTarget = true, AoE = true,
+            SpecOverride = "Restoration",
+        },
+    },
 }
+
